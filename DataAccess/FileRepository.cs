e820fda85f1edd920cd5eb0f85e72022d6fb0f4e
nikita <nikitachumak07@gmail.com>
2026-05-04 01:39:29 +0300

    implement data access layer with auto-folder creation and json serialization

---
+++ DataAccess/FileRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using FinanceManager.Domain;
+
+namespace FinanceManager.DataAccess
+{
+    public class FileRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly string _filePath;
+        private List<T> _items;
+        public FileRepository(string filePath)
+        {
+            _filePath = filePath;
+
+            var directory = Path.GetDirectoryName(_filePath);
+
+            if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            _items = LoadFromFile();
+        }
+        public IEnumerable<T> GetAll() => _items;
+
+        public void Add(T entity)
+        {
+            _items.Add(entity);
+            SaveToFile();
+        }
+
+        public void Update(T entity)
+        {
+            var index = _items.FindIndex(e => e.Id == entity.Id);
+            if (index != -1)
+            {
+                _items[index] = entity;
+                SaveToFile();
+            }
+        }
+        public void Delete(Guid id)
+        {
+            var item = _items.FirstOrDefault(e => e.Id == id);
+            if (item != null)
+            {
+                _items.Remove(item);
+                SaveToFile();
+            }
+        }
+        private List<T> LoadFromFile()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<T>();
+            }
+
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+        }
+        private void SaveToFile()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var json = JsonSerializer.Serialize(_items, options);
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
---
+++ DataAccess/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FinanceManager.Domain;
+
+namespace FinanceManager.DataAccess
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(Guid id);
+
+    }
+}
