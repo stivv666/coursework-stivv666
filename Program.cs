bcd8c2ee2aea222ae64500eb1a99cb8dce8f2048
nikita <nikitachumak07@gmail.com>
2026-05-13 19:48:37 +0300

    integrate finance service with console UI in entry point.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using FinanceManager.DataAccess;
+using FinanceManager.Domain;
+using FinanceManager.Services;
+using FinanceManager.UI;
+
+namespace FinanceManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transactionRepo = new FileRepository<Transaction>("Data/transactions.json");
+            var categoryRepo = new FileRepository<Category>("Data/categories.json");
+
+            var financeService = new FinanceService(transactionRepo, categoryRepo);
+
+            financeService.OnBudgetExceeded += (message) =>
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"\n[ЛІМІТ ПЕРЕВИЩЕНО]: {message}\n");
+                Console.ResetColor();
+            };
+
+            var consoleManager = new ConsoleManager(financeService);
+            consoleManager.Run();
+        }
+    }
+}
